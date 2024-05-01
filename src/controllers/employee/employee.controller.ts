6af8fb5365105d4ba906bbe1ee22ba7d0ff03b87
Yasin Hirani <yasin.hirani@1rivet.com>
2024-05-01 10:34:44 +0530

    Fix : Add project details when getting all employees

--- src/controllers/employee/employee.controller.ts
+++ src/controllers/employee/employee.controller.ts
@@ -5,6 +5,9 @@ import ApiError from "../../utils/apiError";
 import { NextFunction, Request, Response } from "express";
 import prisma from "../../utils/prisma";
 
+/**
+ * Function to get all the employees
+ */
 const getAllEmployees = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     let searchQuery = req.query;
@@ -18,14 +21,35 @@ const getAllEmployees = asyncHandler(
       };
     }
 
-    const employees = await prisma.employee.findMany({
-      where: { ...searchQuery },
-    });
+    let employees = null;
+
+    if (Object.keys(searchQuery).length > 0) {
+      employees = await prisma.employee.findMany({
+        where: { ...searchQuery },
+        include: {
+          assignedProjects: {
+            select: {
+              id: true,
+              allocatedHours: true,
+              startDateOfAllocation: true,
+              endDateOfAllocation: true,
+            },
+          },
+        },
+      });
+    } else {
+      employees = await prisma.employee.findMany({
+        where: { ...searchQuery },
+      });
+    }
 
     res.status(200).json(new ApiResponse({ employees }));
   }
 );
 
+/**
+ * Function to get a single employee
+ */
 const getEmployee = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const employee = await prisma.employee.findUnique({
